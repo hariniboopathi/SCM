fe2b1f33553ee9eaec33db6b9bd24e55092aad0c
hariniboopathi <hariniboopathi1515@gmail.com>
2024-03-07 19:34:40 +0530

    first commit

---
+++ Supply-Chain-Management-starter-file-main/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./Context/**/*.{js,ts,jsx,tsx}",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
